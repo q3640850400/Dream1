00d2bcc23ff31936deaaea25908f13e86fabe149
BeKaLo <910626231@qq.com>
2016-10-18 00:55:16 +0800

    学习Behaviac

---
+++ Assets/MouseCtrl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseCtrl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		BehaviacSystem BS = new BehaviacSystem ();
+		BS.Init ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
