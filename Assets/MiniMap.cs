1a3b31aad796afa79cc7b27d404507f0fc5b4239
BeKaLo <910626231@qq.com>
2016-10-19 23:48:33 +0800

    小地图有头绪了

--- Assets/MiniMap.cs
+++ Assets/MiniMap.cs
@@ -7,15 +7,33 @@ public class MiniMap : MonoBehaviour {
 	private float maxY;
 	private float curX;
 	private float curY;
+	private Camera UICamera;
+	private Vector3 screenPos;
+	private Rect mapRect;
 	// Use this for initialization
 	void Start () {
 		_Model = GameObject.Find ("Model").GetComponent<Model> ();
 		this.maxX = (float)_Model.Mapx;
 		this.maxY = (float)_Model.Mapy;
+		UICamera = GameObject.Find ("UICamera").GetComponent<Camera> ();
+		screenPos = UICamera.WorldToScreenPoint (transform.position);
+		//screenPos.z = 0f;
+		mapRect=new Rect(screenPos.x-100f,screenPos.y-100f,200f,200f);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		test ();
+	}
+	void test(){
+		
+		//Debug.Log (screenPos);
+		if (Input.GetMouseButtonDown (0)) {			
+			//Vector3 mousePos= new Vector2(Input.mousePosition);
+			if (mapRect.Contains (Input.mousePosition)) {
+				Debug.Log ("yyy");
+			}
+		}
 	}
 }
